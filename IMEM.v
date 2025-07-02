5424c30aaafc2cf5c18799f5e72aa6765c16846d
tam3568 <dacnhantam0203@gmail.com>
2025-07-02 16:28:31 +0700

    up

--- IMEM.v
+++ IMEM.v
@@ -2,8 +2,9 @@ module IMEM (
     input  [31:0] addr,
     output [31:0] Instruction
 );
+    parameter IMEM_DEPTH = 101; // Số lệnh thực tế trong imem.hex
     reg [31:0] memory [0:255];
-    assign Instruction = (addr[9:2] < 100) ? memory[addr[9:2]] : 32'hxxxxxxxx;
+    assign Instruction = (addr[9:2] < IMEM_DEPTH) ? memory[addr[9:2]] : 32'hxxxxxxxx;
 
     initial begin
         $readmemh("./mem/imem2.hex", memory);
